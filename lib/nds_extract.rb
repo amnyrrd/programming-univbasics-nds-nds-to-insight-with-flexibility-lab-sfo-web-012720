fc72aa727f1134658788d1442cc6ddffb058bdf3
Aaron Minyard <amnyrrd@gmail.com>
2020-01-24 23:16:55 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -52,7 +52,7 @@ def movies_with_director_key(name, movies_collection)
   result = []
   row = 0
   while row < movies_collection.length
-     result[row] = movie_with_director_name(name, movies_collection[0])
+     result[row] = movie_with_director_name(name, movies_collection[row])
      row += 1
   end
   result
